fcb65c107b9da477e6037509b85eb866f75f8958
ajfm88 <ajfoucaultmo@mail.usf.edu>
2022-04-06 12:38:08 -0700

    Building the About page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import './App.scss';
+import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
-import { Route, Routes } from 'react-router-dom';
-import Layout from './components/Layout';
+import About from './components/About'
+import Layout from './components/Layout'
+import './App.scss'
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Route>
       </Routes>
     </>
-  );
+  )
 }
 
 export default App;
